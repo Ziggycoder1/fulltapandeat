d3a917d6a2829292a8dda361f8bb05474d64bee1
Pudens <125551754+pudens401@users.noreply.github.com>
2025-07-29 14:56:50 +0200

    Updated vite.config.js for hosting

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Important for relative paths on hosting
 })
-
-export default {
-  base: "./", // Important for relative paths on hosting
-};
-
